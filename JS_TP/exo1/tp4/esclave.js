b195d34d0d8b892ad4ec2b8d9a61e0dc734a06d1
FAtole <95278075+FAtole@users.noreply.github.com>
2022-02-02 15:10:52 +0100

    bug

--- JS_TP/exo1/tp4/esclave.js
+++ JS_TP/exo1/tp4/esclave.js
@@ -1,7 +1,6 @@
 onmessage = function (e) {
     console.log('Worker: Message received from main script');
     if (e == "start") {
-        a = { x=1, y=2 };
-        postMessage(a);
+        postMessage([1, 2]);
     }
 }
