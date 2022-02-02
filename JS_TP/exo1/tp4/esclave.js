3b2ab53074ef2126d9a1ac5c139db8c2f6369aa3
FAtole <95278075+FAtole@users.noreply.github.com>
2022-02-02 15:30:28 +0100

    Update esclave.js

--- JS_TP/exo1/tp4/esclave.js
+++ JS_TP/exo1/tp4/esclave.js
@@ -1,7 +1,7 @@
 onmessage = function (e) {
     console.log('Worker: Message received from main script');
-    console.log(e);
-    if (e == "start") {
+    console.log(e.data);
+    if (e.data == "start") {
         postMessage([1, 2]);
         console.log("Envoie message");
     }
