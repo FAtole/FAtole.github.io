fc78ef02ce8e67848e4832de4d31ebd100898315
FAtole <95278075+FAtole@users.noreply.github.com>
2022-02-02 15:45:45 +0100

    color

--- JS_TP/exo1/tp4/Croix.js
+++ JS_TP/exo1/tp4/Croix.js
@@ -1,17 +1,22 @@
 class Croix {
-    constructor(x, y, canvas) {
+    constructor(x, y, canvas,color) {
         this.canvas = canvas;
         this.canvas2dContext = this.canvas.getContext("2d");
         this.x = x;
         this.y = y;
+        this.color=color;
     }
 
     draw() {
-        this.canvas2dContext.fillStyle = "red";
-        this.canvas2dContext.moveTo(x-5, y-5);
-        this.canvas2dContext.lineTo(x+5, y+5);
-        this.canvas2dContext.moveTo(x-5, y+5);
-        this.canvas2dContext.lineTo(x+5, y-5);
+        this.canvas2dContext.beginPath();
+
+        this.canvas2dContext.fillStyle = this.color;
+      //  this.canvas2dContext.fillRect(this.x, this.y, 50, 50);
+        this.canvas2dContext.moveTo(this.x-5, this.y-5);
+        this.canvas2dContext.lineTo(this.x+5, this.y+5);
+        this.canvas2dContext.moveTo(this.x-5, this.y+5);
+        this.canvas2dContext.lineTo(this.x+5, this.y-5);
+        this.canvas2dContext.stroke();
     }
 
 }
